d3675760c00fac5e10831788e09fe212115958d5
Takuiya-dev <example@gmail.com>
2023-09-27 07:12:55 +0900

    Added Page Component and useEffect for showing title on the window tab

--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Container from './Container';
+import Page from './Page';
 
 function HomeGuest() {
 	return (
-		<Container>
+		<Page title='Welcome!' wide={true}>
 			<div className='row align-items-center'>
 				<div className='col-lg-7 py-3 py-md-5'>
 					<h1 className='display-3'>Remember Writing?????</h1>
@@ -35,7 +35,7 @@ function HomeGuest() {
 					</form>
 				</div>
 			</div>
-		</Container>
+		</Page>
 	);
 }
 
--- app/components/Page.js
+++ app/components/Page.js
@@ -1,12 +1,17 @@
 import React, { useEffect } from 'react';
+import Container from './Container';
 
 function Page(props) {
 	useEffect(() => {
-		document.title = `About Us | ComplexApp`;
+		document.title = `${props.title} | ComplexApp`;
 		window.scrollTo(0, 0);
 	}, []);
 
-	return <></>;
+	return (
+		<>
+			<Container wide={props.wide}>{props.children}</Container>
+		</>
+	);
 }
 
 export default Page;
