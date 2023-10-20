f7907f3692350575fea10bdd3301fc4be737451f
Takuiya-dev <example@gmail.com>
2023-10-21 08:19:03 +0900

    Added ProfilePosts component and setup post state, avatar, id, title and date

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -3,6 +3,7 @@ import Page from './Page';
 import { useParams } from 'react-router-dom';
 import Axios from 'axios';
 import StateContext from '../StateContext';
+import ProfilePosts from './ProfilePosts';
 
 function Profile() {
 	const { username } = useParams();
@@ -22,7 +23,7 @@ function Profile() {
 					token: appState.user.token
 				});
 				// console.log(response.data);
-        setProfileData(response.data)
+				setProfileData(response.data);
 			} catch (e) {
 				console.log('There was a problem.');
 			}
@@ -52,32 +53,7 @@ function Profile() {
 				</a>
 			</div>
 
-			<div className="list-group">
-				<a href="#" className="list-group-item list-group-item-action">
-					<img
-						className="avatar-tiny"
-						src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-					/>{' '}
-					<strong>Example Post #1</strong>
-					<span className="text-muted small">on 2/10/2020 </span>
-				</a>
-				<a href="#" className="list-group-item list-group-item-action">
-					<img
-						className="avatar-tiny"
-						src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-					/>{' '}
-					<strong>Example Post #2</strong>
-					<span className="text-muted small">on 2/10/2020 </span>
-				</a>
-				<a href="#" className="list-group-item list-group-item-action">
-					<img
-						className="avatar-tiny"
-						src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-					/>{' '}
-					<strong>Example Post #3</strong>
-					<span className="text-muted small">on 2/10/2020 </span>
-				</a>
-			</div>
+			<ProfilePosts />
 		</Page>
 	);
 }
