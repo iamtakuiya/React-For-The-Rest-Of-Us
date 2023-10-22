4baadb48738f734914267f50a598ca18fb43f617
Takuiya-dev <example@gmail.com>
2023-10-23 00:17:32 +0900

    Added loading text, fetch content, apply dynamic ocntent, reflect tab title, jump back to the profile page.

--- app/components/Page.js
+++ app/components/Page.js
@@ -5,7 +5,7 @@ function Page(props) {
 	useEffect(() => {
 		document.title = `${props.title} | ComplexApp`;
 		window.scrollTo(0, 0);
-	}, []);
+	}, [props.title]);
 
 	return (
 		<>
--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -1,33 +1,66 @@
-import React, { useEffect } from 'react'
-import Page from './Page'
-
+import React, { useEffect, useState } from 'react';
+import Page from './Page';
+import { useParams, Link } from 'react-router-dom';
+import Axios from 'axios';
 
 function ViewSinglePost() {
-  return (
-    <Page title="fake Hardcoded Title">
-<div className="container container--narrow py-md-5">
-      <div className="d-flex justify-content-between">
-        <h2>Example Post Title</h2>
-        <span className="pt-2">
-          <a href="#" className="text-primary mr-2" title="Edit"><i className="fas fa-edit"></i></a>
-          <a className="delete-post-button text-danger" title="Delete"><i className="fas fa-trash"></i></a>
-        </span>
-      </div>
+	const { id } = useParams();
+	const [isLoading, setIsLoading] = useState(true);
+	const [post, setPost] = useState();
+	console.log(post, 'this is post');
 
-      <p className="text-muted small mb-4">
-        <a href="#">
-          <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" />
-        </a>
-        Posted by <a href="#">brad</a> on 2/10/2020
-      </p>
+	useEffect(() => {
+		async function fetchPost() {
+			try {
+				const response = await Axios.get(`/post/${id}`);
+				setPost(response.data);
+				console.table(response.data);
+				setIsLoading(false);
+			} catch (e) {
+				console.log('There was a problem.');
+			}
+		}
 
-      <div className="body-content">
-        <p>Lorem ipsum dolor sit <strong>example</strong> post adipisicing elit. Iure ea at esse, tempore qui possimus soluta impedit natus voluptate, sapiente saepe modi est pariatur. Aut voluptatibus aspernatur fugiat asperiores at.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quod asperiores corrupti omnis qui, placeat neque modi, dignissimos, ab exercitationem eligendi culpa explicabo nulla tempora rem? Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ea at esse, tempore qui possimus soluta impedit natus voluptate, sapiente saepe modi est pariatur. Aut voluptatibus aspernatur fugiat asperiores at.</p>
-      </div>
-    </div>
-    </Page>
-  )
+		fetchPost();
+	}, []);
+
+	if (isLoading) return <Page title="...">Loading...</Page>;
+
+	const date = new Date(post.createdDate);
+	const dateFormatted = `${
+		date.getMonth() + 1
+	}/${date.getDate()}/${date.getFullYear()}`;
+
+	return (
+		<Page title={post.title}>
+			<div className="container container--narrow py-md-5">
+				<div className="d-flex justify-content-between">
+					<h2>{post.title}</h2>
+					<span className="pt-2">
+						<a href="#" className="text-primary mr-2" title="Edit">
+							<i className="fas fa-edit"></i>
+						</a>
+						<a className="delete-post-button text-danger" title="Delete">
+							<i className="fas fa-trash"></i>
+						</a>
+					</span>
+				</div>
+
+				<p className="text-muted small mb-4">
+					<Link to={`/profile/${post.author.username}`}>
+						<img className="avatar-tiny" src={post.author.avatar} />
+					</Link>
+					Posted by{' '}
+					<Link to={`/profile/${post.author.username}`}>
+						${post.author.username}
+					</Link>{' '}
+					{dateFormatted}
+				</p>
+
+				<div className="body-content">{post.body}</div>
+			</div>
+		</Page>
+	);
 }
 
 export default ViewSinglePost;
