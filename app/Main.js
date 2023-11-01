242c4e829f635bbdbe75d59e4ebabe94d168f375
Takuiya-dev <example@gmail.com>
2023-11-02 07:04:01 +0900

    Added CSSTransition for overlay and tooltip each icons

--- app/Main.js
+++ app/Main.js
@@ -2,6 +2,7 @@ import React, { useState, useReducer, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { useImmerReducer } from 'use-immer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
 import Axios from 'axios';
 Axios.defaults.baseURL = 'http://localhost:8080';
 
@@ -89,7 +90,14 @@ function Main() {
 						<Route path="/terms" element={<Terms />} />
 						<Route path="*" element={<NotFound />} />
 					</Routes>
-					{state.isSearchOpen ? <Search /> : ''}
+					<CSSTransition
+						timeout={330}
+						in={state.isSearchOpen}
+						classNames="search-overlay"
+						unmountOnExit
+					>
+						<Search />
+					</CSSTransition>
 					<Footer />
 				</BrowserRouter>
 			</DispatchContext.Provider>
--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -2,6 +2,7 @@ import React, { useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import DispatchContext from '../DispatchContext';
 import StateContext from '../StateContext';
+import { Tooltip as ReactTooltip } from 'react-tooltip';
 
 function HeaderLoggedIn(props) {
 	const appDispatch = useContext(DispatchContext);
@@ -19,22 +20,36 @@ function HeaderLoggedIn(props) {
 	return (
 		<div className="flex-row my-3 my-md-0">
 			<a
+				data-tooltip-id="search"
+				data-tooltip-content="Search"
 				onClick={handleSearchIcon}
 				href="#"
 				className="text-white mr-2 header-search-icon"
 			>
 				<i className="fas fa-search"></i>
 			</a>
-			<span className="mr-2 header-chat-icon text-white">
+			<ReactTooltip place="bottom" id="search" className="custom-tooltip" />{' '}
+			<span
+				data-tooltip-id="chat"
+				data-tooltip-content="Chat"
+				className="mr-2 header-chat-icon text-white"
+			>
 				<i className="fas fa-comment"></i>
 				<span className="chat-count-badge text-white"> </span>
 			</span>
-			<Link to={`/profile/${appState.user.username}`} className="mr-2">
+			<ReactTooltip place="bottom" id="chat" className="custom-tooltip" />{' '}
+			<Link
+				data-tooltip-id="profile"
+				data-tooltip-content="Profile"
+				to={`/profile/${appState.user.username}`}
+				className="mr-2"
+			>
 				<img className="small-header-avatar" src={appState.user.avatar} />
 			</Link>
+			<ReactTooltip place="bottom" id="profile" className="custom-tooltip" />{' '}
 			<Link className="btn btn-sm btn-success mr-2" to="/create-post">
 				Create Post
-			</Link>
+			</Link>{' '}
 			<button onClick={handleLogout} className="btn btn-sm btn-secondary">
 				Sign Out
 			</button>
