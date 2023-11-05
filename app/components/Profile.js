86e761dce76d0f9326f4114d88badace48c1ffe3
Takuiya-dev <example@gmail.com>
2023-11-05 16:57:17 +0900

    Added follow user features

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,19 +1,25 @@
-import React, { useEffect, useContext, useState } from 'react';
+import React, { useEffect, useContext } from 'react';
 import Page from './Page';
 import { useParams } from 'react-router-dom';
 import Axios from 'axios';
 import StateContext from '../StateContext';
 import ProfilePosts from './ProfilePosts';
+import { useImmer } from 'use-immer';
 
 function Profile() {
 	const { username } = useParams();
 	const appState = useContext(StateContext);
-	const [profileData, setProfileData] = useState({
-		profileUsername: '...',
-		profileAvatar:
-			'https://gravatar.com/avatar/b7eb0bea5420aa6a566d3cebe69e2a15?s=128',
-		isFollowing: false,
-		counts: { postCount: '', followerCount: '', followingCount: '' }
+	const [state, setState] = useImmer({
+		followActionLoading: false,
+		startFollowingRequestCount: 0,
+		stopFollowingRequestCount: 0,
+		profileData: {
+			profileUsername: '...',
+			profileAvatar:
+				'https://gravatar.com/avatar/b7eb0bea5420aa6a566d3cebe69e2a15?s=128',
+			isFollowing: false,
+			counts: { postCount: '', followerCount: '', followingCount: '' }
+		}
 	});
 
 	useEffect(() => {
@@ -29,7 +35,10 @@ function Profile() {
 					{ cancelToken: ourRequest.token }
 				);
 				// console.log(response.data);
-				setProfileData(response.data);
+
+				setState((draft) => {
+					draft.profileData = response.data;
+				});
 			} catch (e) {
 				console.log('There was a problem.');
 			}
@@ -38,27 +47,128 @@ function Profile() {
 		return () => {
 			ourRequest.cancel();
 		};
-	}, []);
+	}, [username]);
+
+	useEffect(() => {
+		if (state.startFollowingRequestCount) {
+			setState((draft) => {
+				draft.followActionLoading = true;
+			});
+			const ourRequest = Axios.CancelToken.source();
+
+			async function fetchData() {
+				try {
+					const response = await Axios.post(
+						`/addFollow/${state.profileData.profileUsername}`,
+						{
+							token: appState.user.token
+						},
+						{ cancelToken: ourRequest.token }
+					);
+					// console.log(response.data);
+
+					setState((draft) => {
+						draft.profileData.isFollowing = true;
+						draft.profileData.counts.followerCount++;
+						draft.followActionLoading = false;
+					});
+				} catch (e) {
+					console.log('There was a problem.');
+				}
+			}
+			fetchData();
+			return () => {
+				ourRequest.cancel();
+			};
+		}
+	}, [state.startFollowingRequestCount]);
+
+	useEffect(() => {
+		if (state.stopFollowingRequestCount) {
+			setState((draft) => {
+				draft.followActionLoading = true;
+			});
+			const ourRequest = Axios.CancelToken.source();
+
+			async function fetchData() {
+				try {
+					const response = await Axios.post(
+						`/removeFollow/${state.profileData.profileUsername}`,
+						{
+							token: appState.user.token
+						},
+						{ cancelToken: ourRequest.token }
+					);
+					// console.log(response.data);
+
+					setState((draft) => {
+						draft.profileData.isFollowing = false;
+						draft.profileData.counts.followerCount--;
+						draft.followActionLoading = false;
+					});
+				} catch (e) {
+					console.log('There was a problem.');
+				}
+			}
+			fetchData();
+			return () => {
+				ourRequest.cancel();
+			};
+		}
+	}, [state.stopFollowingRequestCount]);
+
+	function startFollowing() {
+		setState((draft) => {
+			draft.startFollowingRequestCount++;
+		});
+	}
+
+	function stopFollowing() {
+		setState((draft) => {
+			draft.stopFollowingRequestCount++;
+		});
+	}
 
 	return (
 		<Page title="Profile Screen">
 			<h2>
-				<img className="avatar-small" src={profileData.profileAvatar} />
-				{profileData.profileUsername}
-				<button className="btn btn-primary btn-sm ml-2">
-					Follow <i className="fas fa-user-plus"></i>
-				</button>
+				<img className="avatar-small" src={state.profileData.profileAvatar} />
+				{state.profileData.profileUsername}
+				{appState.loggedIn &&
+					!state.profileData.isFollowing &&
+					(appState.user.username != state.profileData.profileUsername) &
+						(state.profileData.profileUsername != '...') && (
+						<button
+							onClick={startFollowing}
+							disabled={state.followActionLoading}
+							className="btn btn-primary btn-sm ml-2"
+						>
+							Follow <i className="fas fa-user-plus"></i>
+						</button>
+					)}
+				{appState.loggedIn &&
+					state.profileData.isFollowing &&
+					(appState.user.username != state.profileData.profileUsername) &
+						(state.profileData.profileUsername != '...') && (
+						<button
+							onClick={stopFollowing}
+							disabled={state.followActionLoading}
+							className="btn btn-danger btn-sm ml-2"
+						>
+							Stop Following <i className="fas fa-user-times"></i>
+						</button>
+					)}
 			</h2>
 
 			<div className="profile-nav nav nav-tabs pt-2 mb-4">
 				<a href="#" className="active nav-item nav-link">
-					Posts: {profileData.counts.postCount}
+					Posts: {state.profileData.counts.postCount}
 				</a>
 				<a href="#" className="nav-item nav-link">
-					Followers: {profileData.counts.followerCount}
+					Followers: {state.profileData.counts.followerCount}
 				</a>
 				<a href="#" className="nav-item nav-link">
-					Following: {profileData.counts.followingCount}
+					Following: {state.profileData.counts.followingCount}
 				</a>
 			</div>
 
--- app/components/ProfilePosts.js
+++ app/components/ProfilePosts.js
@@ -27,7 +27,7 @@ function ProfilePosts() {
 		return () => {
 			ourRequest.cancel();
 		};
-	}, []);
+	}, [username]);
 
 	if (isLoading)
 		return (
